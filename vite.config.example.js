eb282e8ffc22a09ca6636b5e08f5ed57dc0a4873
Ali Barin <ali.barin@camunda.com>
2026-06-03 19:13:53 +0200

    chore(ci): update base path for GitHub Pages deployment

--- vite.config.example.js
+++ vite.config.example.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'example',
+  base: process.env.GITHUB_ACTIONS ? '/cm-theme/' : '/',
   build: {
     outDir: '../demo',
     emptyOutDir: true
